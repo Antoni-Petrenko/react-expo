dde4ccbe3aa9f9ddbe74b764d41d084c8d4feb33
Anton Petrenko <antoni.petrenko.dpua@gmail.com>
2020-07-17 12:36:16 +0300

    version 0.0.1

---
+++ precache-manifest.f534f8cbd2ece77bc7d940b5bad825ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ffcc5ca05f60060f35e075f1880082",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c39a4e78bc61f9e95d88",
+    "url": "/static/css/main.e7d3d6eb.chunk.css"
+  },
+  {
+    "revision": "d46a4128912565dbce00",
+    "url": "/static/js/2.cbd5ca7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cbd5ca7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c39a4e78bc61f9e95d88",
+    "url": "/static/js/main.14ce6e39.chunk.js"
+  },
+  {
+    "revision": "183db94de12628515374",
+    "url": "/static/js/runtime-main.f0911912.js"
+  },
+  {
+    "revision": "81bd4e7c8c523b75e09434b3a13f9046",
+    "url": "/static/media/TTProstoSansRegular.81bd4e7c.otf"
+  }
+]);
